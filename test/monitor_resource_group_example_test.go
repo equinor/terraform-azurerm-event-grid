a904476640c53bc5901cc666813e0be666c41071
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2023-02-17 14:13:24 +0100

    feat: create Event Grid resources

--- test/monitor_resource_group_example_test.go
+++ test/monitor_resource_group_example_test.go
@@ -6,9 +6,9 @@ import (
 	"github.com/gruntwork-io/terratest/modules/terraform"
 )
 
-func TestBasicExample(t *testing.T) {
+func TestMonitorResourceGroupExample(t *testing.T) {
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-		TerraformDir: "../examples/basic",
+		TerraformDir: "../examples/monitor-resource-group",
 	})
 
 	defer terraform.Destroy(t, terraformOptions)
--- variables.tf
+++ variables.tf
@@ -1,3 +1,8 @@
+variable "system_topic_name" {
+  description = "The name of this Event Grid system topic."
+  type        = string
+}
+
 variable "resource_group_name" {
   description = "The name of the resource group to create the resources in."
   type        = string
@@ -6,6 +11,45 @@ variable "resource_group_name" {
 variable "location" {
   description = "The location to create the resources in."
   type        = string
+}
+
+variable "topic_type" {
+  description = "The topic type of the Event Grid system topic."
+  type        = string
+}
+
+variable "source_arm_resource_id" {
+  description = "The ID of the source ARM resource to create this Event Grid system topic for."
+  type        = string
+}
+
+variable "log_analytics_workspace_id" {
+  description = "The ID of the Log Analytics workspace to send diagnostics to."
+  type        = string
+}
+
+variable "event_subscriptions" {
+  description = "A map of event subscription to create for this Event Grid system topic. One of the endpoints must be specified."
+
+  type = map(object({
+    name                 = string
+    included_event_types = optional(list(string))
+
+    azure_function_endpoint = optional(object({
+      function_id = string
+    }))
+
+    storage_queue_endpoint = optional(object({
+      storage_account_id = string
+      queue_name         = string
+    }))
+
+    webhook_endpoint = optional(object({
+      url = string
+    }))
+  }))
+
+  default = {}
 }
 
 variable "tags" {
