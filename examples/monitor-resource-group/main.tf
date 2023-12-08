e36f6f51fd35e1de06dc6f476617bb4d11be8f4c
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2023-12-08 08:12:43 +0100

    docs: development and testing

--- examples/monitor-resource-group/main.tf
+++ examples/monitor-resource-group/main.tf
@@ -6,25 +6,20 @@ resource "random_id" "example" {
   byte_length = 8
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "rg-${random_id.example.hex}"
-  location = var.location
-}
-
 module "log_analytics" {
-  source = "github.com/equinor/terraform-azurerm-log-analytics?ref=v1.5.0"
+  source = "github.com/equinor/terraform-azurerm-log-analytics?ref=v2.1.1"
 
   workspace_name      = "log-${random_id.example.hex}"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = var.resource_group_name
+  location            = var.location
 }
 
 module "storage" {
   source = "github.com/equinor/terraform-azurerm-storage?ref=v10.3.0"
 
   account_name                 = "st${random_id.example.hex}"
-  resource_group_name          = azurerm_resource_group.example.name
-  location                     = azurerm_resource_group.example.location
+  resource_group_name          = var.resource_group_name
+  location                     = var.location
   log_analytics_workspace_id   = module.log_analytics.workspace_id
   shared_access_key_enabled    = true
   network_rules_default_action = "Allow"
@@ -35,12 +30,16 @@ resource "azurerm_storage_queue" "example" {
   storage_account_name = module.storage.account_name
 }
 
+data "azurerm_resource_group" "example" {
+  name = var.resource_group_name
+}
+
 module "event_grid" {
   # source = "github.com/equinor/terraform-azurerm-event-grid"
   source = "../.."
 
   system_topic_name          = "egst-${random_id.example.hex}"
-  resource_group_name        = azurerm_resource_group.example.name
+  resource_group_name        = var.resource_group_name
   location                   = "Global"
   topic_type                 = "Microsoft.Resources.ResourceGroups"
   source_arm_resource_id     = azurerm_resource_group.example.id
@@ -52,7 +51,7 @@ module "event_grid" {
 
       storage_queue_endpoint = {
         storage_account_id = module.storage.account_id
-        queue_name         = azurerm_storage_queue.example.name
+        queue_name         = data.azurerm_storage_queue.example.name
       }
     }
   }
